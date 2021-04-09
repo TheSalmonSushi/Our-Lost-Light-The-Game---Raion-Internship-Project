fc4dc60dd820c9e9abf6777deb9080299acbac39
TheSalmonSushi <dzikrig5@gmail.com>
2021-04-09 15:24:20 +0700

    Add Pause Menu. Add script biar suara background music ga duplicate
    Gw nge add pause menu biar game bisa di pause sama script biar suara background music ga duplicate kalo balik ke main menu

---
+++ Assets/Script/PauseMenu/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI ;
+
+
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused == true)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+      
+    void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+         pauseMenuUI.SetActive(true);
+         Time.timeScale = 0f;
+         GameIsPaused = true;
+    }
+
+}
